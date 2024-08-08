eefa7e353619fa773934224958950fe7f6a50925
zdivyank <divyankzaveri10@gmail.com>
2024-08-08 18:07:59 +0530

    minor change

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'; 
+import React, { useEffect, useRef, useState } from 'react'; 
 import './testimonials.css'; 
 import { CONFIGS } from '../../../config';  
 
